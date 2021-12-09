5013401f147360515f4c606374a9916d980ccd40
Yuhui0215 <00857104@email.ntou.edu.tw>
2021-12-09 13:16:48 +0800

    Initial Commit

---
+++ DataBinding/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DataBinding
+//
+//  Created by User08 on 2021/12/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DataBinding/DataBindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataBindingApp.swift
+//  DataBinding
+//
+//  Created by User08 on 2021/12/9.
+//
+
+import SwiftUI
+
+@main
+struct DataBindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
